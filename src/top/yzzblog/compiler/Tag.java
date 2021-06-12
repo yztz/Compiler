3a3b47ff1726494cff34c473314eecc348e7a4c4
yztz <781089956@qq.com>
2021-06-12 20:32:08 +0800

    c1

--- src/top/yzzblog/compiler/Tag.java
+++ src/top/yzzblog/compiler/Tag.java
@@ -2,9 +2,23 @@ package top.yzzblog.compiler;
 
 public enum Tag {
     ERR,    // 错误
-    END,    //
-    ID,     //
-    KW_INT,
-    NUM,
+    END,    // 文件结束
+    ID,     // 标识符
+    KW_INT, // 数据类型
+    NUM,    // 常量
+    NOT,    // 单目运算符 !
+    ADD, SUB, MUL, DIV, MOD, // 算术运算符
+    GT, GE, LT, LE, EQ, NEQ, //比较运算法
+    AND, OR,                 // 逻辑运算符
+    LPAREN, RPAREN,          // ()
+    LBRACKET, RBRACKET,      // []
+    LBRACE, RBRACE,          // {}
+    COMMA, COLON, SEMICOLON, // , : ;
+    ASSIGN,                  // 赋值
+    KW_IF, KW_ELSE,          // if-else
+    KW_WHILE,                // while
+    KW_CONTINUE,             // continue
+    KW_BREAK,                // break
+    KW_RETURN,               // return
 
 }
--- src/top/yzzblog/compiler/Token.java
+++ src/top/yzzblog/compiler/Token.java
@@ -1,5 +1,15 @@
 package top.yzzblog.compiler;
 
 public class Token {
+    public final Tag tag;
 
+    public Token(Tag tag) {
+        this.tag = tag;
+    }
+
+    static class ID extends Token {
+        public ID(Tag tag) {
+            super(tag);
+        }
+    }
 }
